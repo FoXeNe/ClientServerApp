9f2be348e9e98b30f8a7393f32ae94dba6da8458
FoXeN <dimon20306@mail.ru>
2026-04-27 18:15:56 +0300

    feat(client): execute return CommandResult

--- client/src/main/kotlin/command/Command.kt
+++ client/src/main/kotlin/command/Command.kt
@@ -1,8 +1,10 @@
 package command
 
+import model.CommandResult
+
 interface Command {
     val name: String
     val description: String
 
-    fun execute(args: String)
+    fun execute(args: String): CommandResult
 }
