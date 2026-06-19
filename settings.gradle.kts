d3c3caae8f4620142a967c99400a6c3fcdbd3887
FoXeN <ffoxene@gmail.com>
2026-06-20 01:15:35 +0300

    refactor: minor changes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,5 +10,5 @@ plugins {
     // id("org.gradle.toolchains.foojay-resolver-convention") version "1.0.0"
 }
 
-rootProject.name = "labProg5"
+rootProject.name = "ClientServerApp"
 include("client", "server", "common")
